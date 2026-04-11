921fc19ba494ded27d7dd67871898b67259c62ac
colinperry07 <colin.w.perry@proton.me>
2026-04-10 22:01:01 -0400

    fix: rewrite main to be compatible with the new parser pkg

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -17,55 +12,7 @@ func main() {
 
 	// Logic
 	if *inspect != "" {
-		file, err := os.Open(*inspect)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-
-		for scanner.Scan() {
-
-			line := scanner.Text()
-
-			if !strings.HasPrefix(line, "#") {
-				continue
-			}
-
-			lineId := line[strings.Index(line, "#"):strings.Index(line, "=")]
-			lineType := line[strings.Index(line, "=")+1 : strings.Index(line, "(")]
-			lineParams := line[strings.Index(line, lineType)+len(lineType) : strings.Index(line, ";")]
-
-			strings.ReplaceAll(lineParams, "(", "")
-			strings.ReplaceAll(lineParams, ")", "")
-
-			params := strings.Split(lineParams, ",")
-
-			var paramTypes []string
-
-			switch lineType {
-			case "CARTESIAN_POINT":
-				paramTypes = append(paramTypes, "name", "coordinates")
-			default:
-				paramTypes = nil
-				continue
-			}
-
-			fmt.Printf("%s %s\n", lineType, lineId)
-			for idx, param := range params {
-				if paramTypes != nil {
-					fmt.Printf("%s: %s\n", paramTypes[idx], param)
-				} else {
-					continue
-				}
-			}
-
-		}
-
-		if err := scanner.Err(); err != nil {
-			log.Fatal("Error reading file:", err)
-		}
+		ParseFile(*inspect)
 	}
 
 }
