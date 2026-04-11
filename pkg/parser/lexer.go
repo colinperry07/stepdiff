ef09b87f319f02e8c146a872ab5196ae7424e1c9
colinperry07 <colin.w.perry@proton.me>
2026-04-10 22:19:58 -0400

    fix: alter comments to reflect current state

--- pkg/parser/lexer.go
+++ pkg/parser/lexer.go
@@ -6,17 +6,11 @@ import (
 	"os"
 )
 
-// token identifiers:
-// keywords
-// identifiers
-// literals
-// operators
-// seperators
-
 func GenerateTokens(file *os.File) {
+	// not actually doing anything yet, just prints the .stp file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		fmt.Println(scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
