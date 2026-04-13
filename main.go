00bdd16c049b8174b4ec6b55a78fcd6b35415f02
colinperry07 <colin.w.perry@proton.me>
2026-04-13 18:54:18 -0400

    refactor: clean up commented code and streamline main function logic

--- main.go
+++ main.go
@@ -1,60 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
 
-// . "github.com/colinperry07/stepdiff/pkg/parser"
+	. "github.com/colinperry07/stepdiff/pkg/parser"
+)
 
 func main() {
 
-	// // Command flags
-	// inspect := flag.String("inspect", "", "-inspect=<path/to/file.stp>")
-	// flag.Parse()
+	// Command flags
+	inspect := flag.String("inspect", "", "string=Path to CAD file to inspect")
+	flag.Parse()
 
-	// // Logic
-	// if *inspect != "" {
-	// 	ParseFile(*inspect)
-	// }
-
-	// testing for lexer
-
-	line := "#14=CARTESIAN_POINT('',(-1.06945,-0.92765,0.184));"
-
-	type TOKEN int
-
-	const (
-		POUND = iota
-		NUM
-		IDENT
-		STRING
-		EQUALS
-		LPAREN
-		RPAREN
-		COMMA
-		SEMICOLON
-	)
-
-	var _ = []string{
-		POUND:     "#",
-		NUM:       "NUM",
-		STRING:    "STRING",
-		IDENT:     "IDENT",
-		EQUALS:    "=",
-		LPAREN:    "(",
-		RPAREN:    ")",
-		COMMA:     ",",
-		SEMICOLON: ";",
+	// Logic
+	if *inspect != "" {
+		ParseFile(*inspect)
+	} else {
+		flag.Usage()
 	}
-
-	type Lexer struct {
-		pos   int
-		input string // holds the entire line
-	}
-
-	l := Lexer{pos: 0, input: line}
-
-	for l.pos < len(l.input) {
-		ch := l.input[l.pos]
-		fmt.Println(ch)
-	}
-
 }
