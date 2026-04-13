e6134da1a5e5d01e7381c7733f4c9c193c8eb91d
colinperry07 <colin.w.perry@proton.me>
2026-04-13 00:43:14 -0400

    fix: update ParseFile to use os.ReadFile for improved file handling

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,14 +1,25 @@
 package parser
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func ParseFile(filepath string) {
-	file, err := os.Open(filepath)
+
+	file, err := os.ReadFile(filepath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	GenerateTokens(file)
+
+	l := Lexer{input: file, pos: 0}
+
+	tokens, err := l.Tokenize()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(tokens)
+
 }
