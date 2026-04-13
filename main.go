27d0d054f638af89b4b21bc91b1210980c9a202c
colinperry07 <colin.w.perry@proton.me>
2026-04-13 00:43:05 -0400

    refactor: clean up commented code and enhance lexer structure

--- main.go
+++ main.go
@@ -1,20 +1,60 @@
 package main
 
-import (
-	"flag"
+import "fmt"
 
-	. "github.com/colinperry07/stepdiff/pkg/parser"
-)
+// . "github.com/colinperry07/stepdiff/pkg/parser"
 
 func main() {
 
-	// Command flags
-	inspect := flag.String("inspect", "", "-inspect=<path/to/file.stp>")
-	flag.Parse()
+	// // Command flags
+	// inspect := flag.String("inspect", "", "-inspect=<path/to/file.stp>")
+	// flag.Parse()
 
-	// Logic
-	if *inspect != "" {
-		ParseFile(*inspect)
+	// // Logic
+	// if *inspect != "" {
+	// 	ParseFile(*inspect)
+	// }
+
+	// testing for lexer
+
+	line := "#14=CARTESIAN_POINT('',(-1.06945,-0.92765,0.184));"
+
+	type TOKEN int
+
+	const (
+		POUND = iota
+		NUM
+		IDENT
+		STRING
+		EQUALS
+		LPAREN
+		RPAREN
+		COMMA
+		SEMICOLON
+	)
+
+	var _ = []string{
+		POUND:     "#",
+		NUM:       "NUM",
+		STRING:    "STRING",
+		IDENT:     "IDENT",
+		EQUALS:    "=",
+		LPAREN:    "(",
+		RPAREN:    ")",
+		COMMA:     ",",
+		SEMICOLON: ";",
+	}
+
+	type Lexer struct {
+		pos   int
+		input string // holds the entire line
+	}
+
+	l := Lexer{pos: 0, input: line}
+
+	for l.pos < len(l.input) {
+		ch := l.input[l.pos]
+		fmt.Println(ch)
 	}
 
 }
